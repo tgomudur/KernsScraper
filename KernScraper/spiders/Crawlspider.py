af1790d50577e79ccb9a11a545495decc8df3c07
Tharun <tgomudur@asu.edu>
2016-05-27 21:36:52 -0700

    Update Crawlspider.py

--- KernScraper/spiders/Crawlspider.py
+++ KernScraper/spiders/Crawlspider.py
@@ -17,17 +17,17 @@ class KernSpider(Spider):
 
     def parse(self, response):
         categories = response.xpath("//font[@size='+1']/text()").extract()
+        section = response.xpath("//center/table")[-1]
         print categories
-        tables = response.xpath("//tr[@valign='top']")
+        tables = section.xpath(".//tr[@valign='top']")
         for idx, table in enumerate(tables):
             output_path = self.OUTPUT_DIR + categories[idx] + '/'
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
-            for table in tables:
-                links = table.xpath(".//table//a/@href").extract()
-                composers = table.xpath(".//table//a/text()").extract()
-                for link, composer in zip(links, composers):
-                    yield Request(link, callback=self.parse_link, meta={'parent_dir':output_path + composer + '/'})
+            links = table.xpath(".//table//a/@href").extract()
+            composers = table.xpath(".//table//a/text()").extract()
+            for link, composer in zip(links, composers):
+                yield Request(link, callback=self.parse_link, meta={'parent_dir':output_path + composer + '/'})
 
     def parse_link(self, response):
         parent_dir = response.meta['parent_dir']
@@ -37,14 +37,14 @@ class KernSpider(Spider):
             try:
                 file_url = row.xpath(".//td/a/@href")[1].extract()
                 # print "Scraping" + file_url
-                # filename = row.xpath(".//td/a/text()")[-1].extract().replace(u'\xa0',u' ').encode('utf-8').strip()
+                # filename = row.xpath(".//td/a/text()")[-1].extract().replace(u'\xa0',u' ').encode('utf-8')
                 filename = file_url.split('/')[-1].split('&')[1].split("=")[-1]
                 yield Request(file_url, callback=self.parse_song1, meta={'path':parent_dir + filename})
             except:
                 continue
 
     def parse_song1(self, response):
-        filename = response.meta['path'] + '.txt'
+        filename = response.meta['path']
         # print "Filename "+ filename
         if not os.path.isfile(filename):
             with open(filename, 'w') as f:
